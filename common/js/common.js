0d32e10f559ed20dc8f73a3ccd4ddda10310ecf0
195-watanabe <19n.oshigoto@gmail.com>
2023-07-03 03:37:15 +0900

    TOPページPC完成

--- common/js/common.js
+++ common/js/common.js
@@ -4,3 +4,23 @@ $(".hamburger-btn").click(function () {//ボタンがクリックされたら
   $(".hamburger-btn-center").toggleClass('active');//ボタン自身に activeクラスを付与し
   $(".hamburger-drawer").fadeToggle(200);//ボタン自身に activeクラスを付与し
 });
+
+// TOPページのプロジェクトピックアップのスライド
+$('.pickup-slide').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 800,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: false,
+  prevArrow: '<img src="../../common/images/btn_pickup_prev.png" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="../../common/images/btn_pickup_next.png" class="slide-arrow next-arrow">',
+
+  responsive: [{
+   breakpoint: 767, // ブレイクポイントを指定
+    settings: {
+     slidesToShow: 1,
+    },
+   }
+ ]
+});
